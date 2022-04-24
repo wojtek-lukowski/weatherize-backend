1ac95132aa7f8b044b2e6ea13b259c685371bd86
Wojtek Lukowski <wojtek_lukowski@yahoo.com>
2022-04-24 14:26:53 +0200

    heroku port

--- index.js
+++ index.js
@@ -145,12 +145,12 @@ app.use((err, req, res, next) => {
   res.status(500).send('Something broke');
 });
 
-app.listen(8080, () => {
-  console.log('Weatherize backend listening on port 8080');
-});
-
-// const port = process.env.PORT || 8080;
-// app.listen(port, '0.0.0.0.',() => {
-//   console.log('Weatherize backend listening on port', port);
+// app.listen(8080, () => {
+//   console.log('Weatherize backend listening on port 8080');
 // });
+
+const port = process.env.PORT || 8080;
+app.listen(port, '0.0.0.0.',() => {
+  console.log('Weatherize backend listening on port', port);
+});
 
