620202013cf3384e1b2961f4c4754f7047c44bbf
Wojtek Lukowski <wojtek_lukowski@yahoo.com>
2022-04-25 10:55:45 +0200

    CORS on

--- index.js
+++ index.js
@@ -21,17 +21,17 @@ let auth = require('./auth')(app);
 const passport = require('passport');
 require('./passport');
 
-// let allowedOrigins = ['http://localhost:8080', 'http://localhost:3000', 'https://wojtek-lukowski.github.io']
-// app.use(cors({
-//   origin: (origin, callback) => {
-//     if(!origin) return callback(null, true);
-//     if(allowedOrigins.indexOf(origin) === -1) {
-//       let message = 'The CORS policy for this application does not allow access from origin', origin;
-//       return callback(new Error(message ), false);
-//     }
-//     return callback(null, true);
-//   }
-// }));
+let allowedOrigins = ['http://localhost:8080', 'http://localhost:3000', 'https://wojtek-lukowski.github.io']
+app.use(cors({
+  origin: (origin, callback) => {
+    if(!origin) return callback(null, true);
+    if(allowedOrigins.indexOf(origin) === -1) {
+      let message = 'The CORS policy for this application does not allow access from origin', origin;
+      return callback(new Error(message ), false);
+    }
+    return callback(null, true);
+  }
+}));
 
 app.get('/', (req, res )=> {
   res.send('Weatherize backend index.js');
